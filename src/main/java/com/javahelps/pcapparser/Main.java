6821108f695d1969f17f4bb9e526a340039de87f
David Alain <davidalain89@gmail.com>
2017-12-10 20:28:55 -0300

    Criada classe PacketUtil.java para ajudar na leitura dos campos dos cabeçalhos.

--- src/main/java/com/javahelps/pcapparser/Main.java
+++ src/main/java/com/javahelps/pcapparser/Main.java
@@ -14,27 +14,26 @@ import io.pkts.protocol.Protocol;
 
 public class Main {
 
-	public static final int ETHERNET_HEADER_SIZE = 14;
-	public static final int IP_HEADER_SIZE = 20;
+//	public static void main2(String[] args) throws IOException {
+//
+//		byte[] a = {(byte) 0x80, 0x10, 0x04, (byte) 0xB2, 0x58, 0x4E, 0x00, 0x00};
+//		byte[] b = {0x01, 0x01, 0x08, 0x0A, (byte) 0xA7, (byte) 0xB8, 0x20, 0x7F};
+//
+//		for(int i = 0 ; i < a.length ; i++) {
+//			System.out.println("c="+HexPrinter.toHexString(a[i]) + "__"+(new String(a,i,1))+"__");	
+//		}
+//
+//		for(int i = 0 ; i < b.length ; i++) {
+//			System.out.println("c="+HexPrinter.toHexString(b[i]) + "__"+(new String(b,i,1))+"__");	
+//		}
+//
+//	}
 
-	public static void main2(String[] args) throws IOException {
-
-		byte[] a = {(byte) 0x80, 0x10, 0x04, (byte) 0xB2, 0x58, 0x4E, 0x00, 0x00};
-		byte[] b = {0x01, 0x01, 0x08, 0x0A, (byte) 0xA7, (byte) 0xB8, 0x20, 0x7F};
-
-		for(int i = 0 ; i < a.length ; i++) {
-			System.out.println("c="+HexPrinter.toHexString(a[i]) + "__"+(new String(a,i,1))+"__");	
-		}
-
-		for(int i = 0 ; i < b.length ; i++) {
-			System.out.println("c="+HexPrinter.toHexString(b[i]) + "__"+(new String(b,i,1))+"__");	
-		}
-
-	}
-
+	private static int packetNumber = 0;
+	
 	public static void main(String[] args) throws IOException {
 
-		final Pcap pcap = Pcap.openStream("trace.pcap");
+		final Pcap pcap = Pcap.openStream("trace (1).pcap");
 
 		pcap.loop(new PacketHandler() {
 			@Override
@@ -42,19 +41,25 @@ public class Main {
 
 				if (packet.hasProtocol(Protocol.ETHERNET_II)) {
 					System.out.println("============================================================");
+					System.out.println("packetNumber: " + packetNumber++);
 
 					MACPacket ethernetPacket = (MACPacket) packet.getPacket(Protocol.ETHERNET_II);
 					Buffer ethernetBuffer = ethernetPacket.getPayload();
 					byte[] ethernetBufferArray = ethernetBuffer.getArray();
 					int ethernetPayloadLen = ethernetBufferArray.length;
 					
+					System.out.println("time(us): " + PacketUtil.getArrivalTime(packet));
+					System.out.println("IP: "
+							+ PacketUtil.getSourceIP(packet) 		+ ":" + PacketUtil.getSourcePort(packet) + " -> "
+							+ PacketUtil.getDestinationIP(packet) 	+ ":" + PacketUtil.getDestinationPort(packet));
+					
 					System.out.println("==== MAC: ===");
 					System.out.println("Ethernet frame size: " + ethernetPacket.getTotalLength());
 					System.out.println("Ethernet header size: " + (ethernetPacket.getTotalLength() - ethernetPayloadLen));
 					System.out.println("Ethernet payload size: " + ethernetPayloadLen);
 					
-					System.out.println(ethernetBufferArray.length);
-					System.out.println(HexPrinter.toStringHexFormatted(ethernetBufferArray));
+//					System.out.println(ethernetBufferArray.length);
+//					System.out.println(HexPrinter.toStringHexFormatted(ethernetBufferArray));
 					
 					if (ethernetPacket.hasProtocol(Protocol.IPv4)) {
 
@@ -68,8 +73,8 @@ public class Main {
 						System.out.println("IP header size: " + (ethernetPayloadLen - ipPayloadLen));
 						System.out.println("IP payload size: " + ipPayloadLen);
 
-						System.out.println(ipBufferArray.length);
-						System.out.println(HexPrinter.toStringHexFormatted(ipBufferArray));
+//						System.out.println(ipBufferArray.length);
+//						System.out.println(HexPrinter.toStringHexFormatted(ipBufferArray));
 
 						if (ipPacket.hasProtocol(Protocol.TCP)) {
 
@@ -86,13 +91,9 @@ public class Main {
 								System.out.println("TCP header size: " + (ipPayloadLen - tcpPayloadLen));
 								System.out.println("TCP payload size: " + tcpPayloadLen);
 
-								System.out.println("time(us): " + tcpPacket.getArrivalTime());
-								System.out.println("IP: "
-										+ tcpPacket.getSourceIP() 		+ ":" + tcpPacket.getSourcePort() + " -> "
-										+ tcpPacket.getDestinationIP() 	+ ":" + tcpPacket.getDestinationPort());
-
 								System.out.println(tcpBufferArray.length);
 								System.out.println(HexPrinter.toStringHexFormatted(tcpBufferArray));
+								System.out.println(tcpBuffer);
 							}
 
 						} else if (packet.hasProtocol(Protocol.UDP)) {
---
+++ src/main/java/com/javahelps/pcapparser/PacketUtil.java
@@ -0,0 +1,66 @@
+package com.javahelps.pcapparser;
+
+import java.io.IOException;
+
+import io.pkts.packet.IPPacket;
+import io.pkts.packet.Packet;
+import io.pkts.packet.TCPPacket;
+import io.pkts.packet.UDPPacket;
+import io.pkts.protocol.Protocol;
+
+public class PacketUtil {
+
+	public static long getArrivalTime(Packet packet) {
+		return packet.getArrivalTime();
+	}
+
+	public static String getSourceIP(Packet packet) throws IOException {
+
+		if(!packet.hasProtocol(Protocol.IPv4))
+			return null;
+
+		IPPacket ip = (IPPacket) packet.getPacket(Protocol.IPv4);
+		return ip.getSourceIP();
+	}
+
+	public static String getDestinationIP(Packet packet) throws IOException {
+
+		if(!packet.hasProtocol(Protocol.IPv4))
+			return null;
+
+		IPPacket ip = (IPPacket) packet.getPacket(Protocol.IPv4);
+		return ip.getDestinationIP();
+	}
+
+	public static int getSourcePort(Packet packet) throws IOException {
+
+		if(packet.hasProtocol(Protocol.TCP)) {
+			TCPPacket ip = (TCPPacket) packet.getPacket(Protocol.TCP);
+			return ip.getSourcePort();
+		}
+		
+		if(packet.hasProtocol(Protocol.UDP)) {
+			UDPPacket ip = (UDPPacket) packet.getPacket(Protocol.UDP);
+			return ip.getSourcePort();
+		}
+		
+		return -1;
+	}
+	
+	public static int getDestinationPort(Packet packet) throws IOException {
+
+		if(packet.hasProtocol(Protocol.TCP)) {
+			TCPPacket ip = (TCPPacket) packet.getPacket(Protocol.TCP);
+			return ip.getDestinationPort();
+		}
+		
+		if(packet.hasProtocol(Protocol.UDP)) {
+			UDPPacket ip = (UDPPacket) packet.getPacket(Protocol.UDP);
+			return ip.getDestinationPort();
+		}
+		
+		return -1;
+	}
+
+
+}
